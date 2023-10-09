9a0f4a19d9edb7ca598d59a1ba55b319c9aaa09b
Zeuska90 <zeus.ka90@gmail.com>
2023-10-09 16:15:09 +0200

    add payment method

---
+++ ShopOnline.Web/Pages/CheckoutBase.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Pages
+{
+    public class CheckoutBase : ComponentBase
+    {
+        [Inject]
+        public IJSRuntime Js { get; set; } 
+        protected IEnumerable<CartItemDto> ShoppingCartItems { get; set; }
+        protected string PaymentDescription { get; set; }
+        protected decimal PaymentAmount { get; set; }
+        protected int TotalQty { get; set; }
+        [Inject]
+        public IShoppingCartService ShoppingCartService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                ShoppingCartItems = await ShoppingCartService.GetAllItems(HardCoded.UserId);
+
+                if (ShoppingCartItems != null)
+                {
+                    Guid paymentGuid = Guid.NewGuid();
+
+                    PaymentAmount = ShoppingCartItems.Sum(p => p.TotalPrice);
+                    TotalQty = ShoppingCartItems.Sum(p => p.Qty);
+                    PaymentDescription = $"O_{HardCoded.UserId}_{paymentGuid}";
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            //if event occured after first render
+            
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            try
+            {
+                //use argument to ensure that the code in this method fired only once after component is renderd
+                if (firstRender)
+                {
+                    //call js method
+                    await Js.InvokeVoidAsync("initPayPalButton");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
